317724f3f2691eab0b7fb657bfb29a45db87f6a8
Rafael George <george.rafael@gmail.com>
2015-12-07 08:48:21 -0500

    Add new structure to represent the returned json from the API

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "net/http"
 
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
 func main() {
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":8080", nil)
